8d21b1bc64368a04d45b1c36f9071fe0482cfad4
reyhaneh <reyhanehrm333@gmail.com>
2021-12-23 21:18:41 +0330

    used scss

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import style from "./main.css";
+import style from "./style.scss";
 import component from "./component.js";
 import { shadowizard } from "shadowizardr";
 document.body.append(component());
